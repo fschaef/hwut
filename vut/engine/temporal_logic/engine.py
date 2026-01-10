ccc3f77947775d757558a003a5d1a4ab58bc068e
Frank-Rene Schaefer <fschaef@users.sourceforge.net>
2026-01-10 13:44:32 +0100

    unrelated -- temporal logic brainstorming

--- vut/engine/temporal_logic/engine.py
+++ vut/engine/temporal_logic/engine.py
@@ -1,9 +1,46 @@
 from __future__ import annotations
 
+from itertools   import chain
 from dataclasses import dataclass
+
+class Attribute:
+    value: Any
+    def is_equivalent(other: Attribute):
+        # Note, that may be defined tolerant, an attribute 'diameter' may consider a deviation of 1e-6 as 'nothing'
+        pass
+
+@dataclass
+class Now(dict[str,list[Event]]):
+    def has(self, event_spec):
+        """RETURNS: True, If any event living in now fits the given specification
+                    False, else.
+        """
+        return any(attribute.is_equivalent(event.get(attribute.name, None))
+                   for event in self.get(event_spec.name, [])
+                   for attribute in event_spec.attributes)
+
+@dataclass
+class Event:
+    name:       str
+    attributes: dict[str, Any]
+    
+@dataclass
+class TScope:
+    on_event:    Event
+    until_event: Event
+
+    def react(self, now: Now):
+        """RETURNS: True,  'activating'    <= an event in Now
+                    False, 'de-activating' <= an event int Now
+                    None   indifferent with anything in Now
+        """
+        if   now.has(self.on_event):    return True
+        elif now.has(self.until_event): return False
+        else:                           return None
 
 @dataclass
 class RuleSet:
+    tscope:    TScope             # temporal scope
     on_entry:  LuaCompiledCode
     on_exit:   LuaCompiledCode
     rule_list: RuleList
@@ -15,14 +52,19 @@ class E_TemporalLogicMode(Enum):
 @dataclass
 class Engine:
     mode:               E_TemporalLogicMode
+
+    state_space:        StateSpace
+
+    rule_set_manager:   RuleSetManager
+
     event_stream_api:   EventStreamAPI
-    meta_event_db:      EventDefDB
-    history:            TriggerHistory
 
-    rule_space:         RuleSpace
+    def initialize(self, 
+                   mode:               E_TemporalLogicMode, 
+                   header_source_file: str):
+        self.mode = mode
+        self.state_space.initialize(self.boot_strap_file_name, mode)
 
-    def initialize(self, header_source_file):
-        self.rule_space.initialize(self.boot_strap_file_name)
         # Lua-header with convenience functions, enum definitions etc.
         self.parse_test_specific_lua_header(header_source_file)
         # Event definitions:
@@ -30,44 +72,115 @@ class Engine:
         #    EVENT-structure specifications
         self.parse_test_specific_event_definitions(header_source_file)
 
-    def _apply(self, event_list: list[Event], time_sec: float):
-        self.now          = self.meta_event_db(event) # determine what events are in 'now'
-        active_rule_sets,  \
-        on_entry_handlers, \
-        on_exit_handlers   = self.rule_space.apply(now)
+    def _jiffy_step(self, event: Event, time_sec: float):
+        """RETURNS: [0] True, if no violation happend; False, else.
+                    [1] sorted list of actions to be considered as a consequence of
+                        rule sets being activated by events of now.
 
-        verdict = self.rule_space.judge(active_rule_sets, self.mode)
+        The list of actions is sorted by provenance, as it defines the order 
+        of execution.
+        """
+        # 'now' = set of events that exist in this particular jiffy
+        now = self.event_space.create_now(event, self.history) 
+        self.history.register_now(now, time_sec)
+
+        active_rule_sets,     \
+        activated_rule_sets,  \
+        deactivated_rule_sets = self.rule_set_manager.apply(now, self.history, time_sec)
+
+        verdict = self.state_space.judge(active_rule_sets)
         if verdict is False and self.mode is E_TemporalLogicMode.COMPLIANCE:
             return None
 
-        return [ 
-            handler
-            for handler in chain([rset.on_entry for rset in activated], 
-                                 [rset.on_exit  for rset in deactivated])
-        ]
+        def provenance_priority(action):
+            """Key for sorting the actions: A 'file priority index' gives the
+            priority of the file where the action is located. 'line_n' is the 
+            number of the line where the action has been specified.
+            """
+            return action.file_priority_index, action.line_n
+
+        return sorted((chain([rset.on_entry for rset in activated_rule_sets], 
+                             [rset.on_exit  for rset in deactivated_rule_sets])),
+                      key = provenance_priority)
 
     def process_external_event(self, event: Event, time_sec: float):
-        work_list = [[event]]
+        """RETURNS: True, if no violation occurred
+                    False, if a rule violation occurred
+
+        If the engine is setup in COMPLIANCE mode, a violation caused a immediate
+        abort of the process. Else, it continues in order to run through all 
+        jiffy steps related to an external event.
+        """
+        verdict   = True
+        work_list = [ self.rule_space.new_now_by_event(event) ]
 
         while work_list:
-            if (result := self._apply([event], time_sec)) is None: 
-                return False
+            now = work_list.pop()
 
-            handler_list = result 
+            sub_verdict,        \
+            sorted_action_list = self._jiffy_step(now, time_sec)
 
-            for action in sorted_action_sequence(handler_list):
-                event_list = self.rule_space.process_action(action)
-                work_list.append(event_list)
+            if sub_verdict is False:
+                verdict = False
+                if self.mode == E_TemporalLogicMode.COMPLIANCE: break
 
-        return True
+            for action in sorted_action_list:
+                work_list.append(self.state_space.new_now_by_action(action))
+
+        return verdict
+
+class LuaSpace:
+    lua_executer: Any
+
+    def do(self, action):
+        """RETURNS: [0] events triggered directly by action's actions
+                    [1] state changes in the lua space, caused by action's actions
+        """
+        # snapshot the hidden object space with respect to all objects 
+        # possibly subject to change
+        relevant = self.lua_inspector.get_affected_objects(action)
+
+        # before: state of objects subject to change by action, before execution
+        # after:  state of objects subject to change by action, after execution
+        before           = self.hidden_object_space.snapshot(relevant)
+        triggered_events = self.lua_executer.do(action)
+        after            = self.hidden_object_space.snapshot(relevant)
+
+        state_changes    = self.determine_state_changes(before, after)
+
+        return triggered_events, state_changes
 
 @dataclass
-class RuleSpace:
-    tscope_to_rule_db:  TscopeDb
-    lua_space:          LuaSpace
+class EventSpace:
+    history:   History
+
+@dataclass
+class StateSpace:
+    event_space:         EventSpace
+    hidden_object_space: LuaSpace
+
     def initialize(self, bootstrap_file):
         self.initialize_lua()
         self.parse_bootstrap(bootstrap_file)
+
+    def new_now_by_event(self, event, time: float) -> Now:
+        """RETURNS: a new 'now' = list of events
+
+        Given the meta-events in the event space, an event may imply other
+        events. This function determines all implied events in order to define
+        the set of all events at this particular 'now'.
+        """
+        return self.event_space.imply([event], time)
+
+    def new_now_by_action(self, action, time: float) -> Now:
+        """RETURNS: a new 'now' = list of events
+
+        Executes action, collects the events it triggered and determines what
+        events are triggered by the state changes it performed.
+        """
+        events, state_changes = self.hidden_object_space.do(action)
+
+        return self.event_space.imply(events, time, state_changes)
 
 @dataclass
 class EventStreamAPI:
