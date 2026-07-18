99f0518f91902dc2ecb216a5866cc54f3d398e54
Frank-Rene Schaefer <fschaef@users.sourceforge.net>
2026-07-18 07:47:04 +0200

    sealed temporal engine language

--- vut/engine/temporal_logic-previous/parser/__init__.py
+++
@@ -1,4 +0,0 @@
-"""Parser package. Importing it has NO side effects: grammar registration and
-compilation happen lazily in rule_parser.compiled_grammar(), so this package can
-be imported without forming the grammar -> Luau Role -> parser.core import cycle.
-"""
--- vut/engine/temporal_logic-previous/world/luau/TEST/call_luau.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-# SPDX-License: MIT; Project VUT; (C) Frank-Rene Schaefer
-#
-# PURPOSE: shebang/launcher wrapper so HWUT's call convention reaches Luau.
-script=$1; shift
-exec luau "$script" -a "$@"
